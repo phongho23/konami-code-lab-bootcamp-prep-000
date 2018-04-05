24f1560aa43af2f58511259c99b2ccb5306e9e26
Phong Ho <ho.phongt@gmail.com>
2018-04-05 01:10:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
 
-document.addEventListener
+document.addEventListener(keydown)
 const key = parseInt(e.detail || e.which);
 
 if (key === code[index]) {
