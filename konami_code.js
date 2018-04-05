caf417f4978688dbea03e8961f46202946351d70
Phong Ho <ho.phongt@gmail.com>
2018-04-05 01:09:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index = 0;
 
-function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
 
+document.body.
 const key = parseInt(e.detail || e.which);
 
 if (key === code[index]) {
