5d619b6f25af44e1dcc233f4c71c3f06147aa679
Phong Ho <ho.phongt@gmail.com>
2018-04-05 00:54:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,19 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 
 
 
